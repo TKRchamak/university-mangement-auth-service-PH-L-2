b335396ee1e4ed4f50c9f4787388544e89172fac
TKRchamak <tonmoykumarroy6@gmail.com>
2024-04-14 20:44:37 +0600

    [Update] user module

--- src/app/modules/users/users.interface.ts
+++ src/app/modules/users/users.interface.ts
@@ -1,14 +1,14 @@
-import { Model } from 'mongoose'
+import { Model } from 'mongoose';
 
 export type IUser = {
-  id: string
-  role: string
-  password: string
-}
+  id: string;
+  role: string;
+  password: string;
+};
 
 // export type UserMethods = {
 //   fullName: () => void
 // }
 
 // export type UserModel = Model<IUser, object, UserMethods>
-export type UserModel = Model<IUser, object>
+export type UserModel = Model<IUser, object>;
